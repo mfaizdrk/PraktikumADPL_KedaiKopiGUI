30b38b5f78453056315c04ab4fc9f09661c58e66
mfaizdrk <faiz.dwi2882@gmail.com>
2026-09-23 21:55:42 +0700

    KedaiKopiGUI

---
+++ KedaiKopiGUI/src/main/java/com/adpl/kedaikopigui/KedaiKopiGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.adpl.kedaikopigui;
+
+/**
+ *
+ * @author faizd
+ */
+public class KedaiKopiGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
